82d4c832ac61676012bab2fd3cc605e5de2c96df
Sebastián Becerra <sebaebc@SebaEBC-Macbook-Air.local>
2020-12-20 14:02:40 -0300

    initial version 0.0.1

---
+++ ag-grid-es-CL.ts
@@ -0,0 +1,247 @@
+/**
+ * agGrid Spanish (es-CL) locale, based on the default localisation file of AgGrid
+ * 
+ * @author SebaEBC
+ * @date 15-12-2020
+ * @version 0.0.1
+ */
+
+export const AG_GRID_LOCALE_ES = {
+    // Set Filter
+    selectAll: 'Seleccionar Todo',
+    selectAllSearchResults: '(Select All Search Results)',
+    searchOoo: 'Buscar...',
+    blanks: 'En blanco',
+    noMatches: 'No matches',
+
+    // Number Filter & Text Filter
+    filterOoo: 'Filtrar...',
+    equals: 'Igual',
+    notEqual: 'No es igual',
+    empty: 'Choose One',
+
+    // Number Filter
+    lessThan: 'Menos que',
+    greaterThan: 'Mayor que',
+    lessThanOrEqual: 'Menos o igual que',
+    greaterThanOrEqual: 'Mayor o igual que',
+    inRange: 'En rango de',
+    inRangeStart: 'to',
+    inRangeEnd: 'from',
+
+    // Text Filter
+    contains: 'Contiene',
+    notContains: 'No contiene',
+    startsWith: 'Empieza con',
+    endsWith: 'Termina con',
+
+    // Date Filter
+    dateFormatOoo: 'yyyy-mm-dd',
+
+    // Filter Conditions
+    andCondition: 'Y',
+    orCondition: 'O',
+
+    // Filter Buttons
+    applyFilter: 'Aplicar Filtro...',
+    resetFilter: 'Reestablecer',
+    clearFilter: 'Limpiar',
+    cancelFilter: 'Cancelar',
+
+    // Filter Titles
+    textFilter: 'Text Filter',
+    numberFilter: 'Number Filter',
+    dateFilter: 'Date Filter',
+    setFilter: 'Set Filter',
+
+    // Side Bar
+    columns: 'Columns',
+    filters: 'Filters',
+
+    // columns tool panel
+    pivotMode: 'Modo Pivote',
+    groups: 'Grupos',
+    rowGroupColumnsEmptyMessage: 'Drag here to set row groups',
+    values: 'Valores',
+    valueColumnsEmptyMessage: 'Drag here to aggregate',
+    pivots: 'Pivotes',
+    pivotColumnsEmptyMessage: 'Drag here to set column labels',
+
+    // Header of the Default Group Column
+    group: 'Grupo',
+
+    // Other
+    loadingOoo: 'Cargando...',
+    noRowsToShow: 'No hay filas para mostrar',
+    enabled: 'Enabled',
+
+    // Menu
+    pinColumn: 'Columna Pin',
+    pinLeft: 'Pin Izquierdo',
+    pinRight: 'Pin Derecho',
+    noPin: 'No Pin',
+    valueAggregation: 'Agregar valor',
+    autosizeThiscolumn: 'Ajustar esta columna',
+    autosizeAllColumns: 'Ajustar todas las columnas',
+    groupBy: 'agrupar',
+    ungroupBy: 'desagrupar',
+    resetColumns: 'Reestablecer Columnas',
+    expandAll: 'Expandir todo',
+    collapseAll: 'Colapsar todo',
+    copy: 'Copiar',
+    ctrlC: 'Ctrl+C',
+    copyWithHeaders: 'Copiar con cabeceras',
+    paste: 'Pegar',
+    ctrlV: 'Ctrl+V',
+    export: 'Exportar',
+    csvExport: 'Exportar a CSV',
+    excelExport: 'Exportar a Excel (.xlsx)',
+    excelXmlExport: 'Exportar a Excel (.xml)',
+
+    // Enterprise Menu Aggregation and Status Bar
+    sum: 'Suman',
+    min: 'Minimo',
+    max: 'Maximo',
+    none: 'nada',
+    count: 'contar',
+    avg: 'promedio',
+    filteredRows: 'Filtered',
+    selectedRows: 'Selected',
+    totalRows: 'Total Rows',
+    totalAndFilteredRows: 'Rows',
+    more: 'More',
+    to: 'to',
+    of: 'of',
+    page: 'Page',
+    nextPage: 'Next Page',
+    lastPage: 'Last Page',
+    firstPage: 'First Page',
+    previousPage: 'Previous Page',
+
+    // Enterprise Menu (Charts)
+    pivotChartAndPivotMode: 'Pivot Chart & Pivot Mode',
+    pivotChart: 'Pivot Chart',
+    chartRange: 'Chart Range',
+
+    columnChart: 'Column',
+    groupedColumn: 'Grouped',
+    stackedColumn: 'Stacked',
+    normalizedColumn: '100% Stacked',
+
+    barChart: 'Bar',
+    groupedBar: 'Grouped',
+    stackedBar: 'Stacked',
+    normalizedBar: '100% Stacked',
+
+    pieChart: 'Pie',
+    pie: 'Pie',
+    doughnut: 'Doughnut',
+
+    line: 'Line',
+
+    xyChart: 'X Y (Scatter)',
+    scatter: 'Scatter',
+    bubble: 'Bubble',
+
+    areaChart: 'Area',
+    area: 'Area',
+    stackedArea: 'Stacked',
+    normalizedArea: '100% Stacked',
+
+    histogramChart: 'Histogram',
+
+    // Charts
+    pivotChartTitle: 'Pivot Chart',
+    rangeChartTitle: 'Range Chart',
+    settings: 'Settings',
+    data: 'Data',
+    format: 'Format',
+    categories: 'Categories',
+    defaultCategory: '(None)',
+    series: 'Series',
+    xyValues: 'X Y Values',
+    paired: 'Paired Mode',
+    axis: 'Axis',
+    navigator: 'Navigator',
+    color: 'Color',
+    thickness: 'Thickness',
+    xType: 'X Type',
+    automatic: 'Automatic',
+    category: 'Category',
+    number: 'Number',
+    time: 'Time',
+    xRotation: 'X Rotation',
+    yRotation: 'Y Rotation',
+    ticks: 'Ticks',
+    width: 'Width',
+    height: 'Height',
+    length: 'Length',
+    padding: 'Padding',
+    spacing: 'Spacing',
+    chart: 'Chart',
+    title: 'Title',
+    titlePlaceholder: 'Chart title - double click to edit',
+    background: 'Background',
+    font: 'Font',
+    top: 'Top',
+    right: 'Right',
+    bottom: 'Bottom',
+    left: 'Left',
+    labels: 'Labels',
+    size: 'Size',
+    minSize: 'Minimum Size',
+    maxSize: 'Maximum Size',
+    legend: 'Legend',
+    position: 'Position',
+    markerSize: 'Marker Size',
+    markerStroke: 'Marker Stroke',
+    markerPadding: 'Marker Padding',
+    itemSpacing: 'Item Spacing',
+    itemPaddingX: 'Item Padding X',
+    itemPaddingY: 'Item Padding Y',
+    layoutHorizontalSpacing: 'Horizontal Spacing',
+    layoutVerticalSpacing: 'Vertical Spacing',
+    strokeWidth: 'Stroke Width',
+    offset: 'Offset',
+    offsets: 'Offsets',
+    tooltips: 'Tooltips',
+    callout: 'Callout',
+    markers: 'Markers',
+    shadow: 'Shadow',
+    blur: 'Blur',
+    xOffset: 'X Offset',
+    yOffset: 'Y Offset',
+    lineWidth: 'Line Width',
+    normal: 'Normal',
+    bold: 'Bold',
+    italic: 'Italic',
+    boldItalic: 'Bold Italic',
+    predefined: 'Predefined',
+    fillOpacity: 'Fill Opacity',
+    strokeOpacity: 'Line Opacity',
+    histogramBinCount: 'Bin count',
+    columnGroup: 'Column',
+    barGroup: 'Bar',
+    pieGroup: 'Pie',
+    lineGroup: 'Line',
+    scatterGroup: 'X Y (Scatter)',
+    areaGroup: 'Area',
+    histogramGroup: 'Histogram',
+    groupedColumnTooltip: 'Grouped',
+    stackedColumnTooltip: 'Stacked',
+    normalizedColumnTooltip: '100% Stacked',
+    groupedBarTooltip: 'Grouped',
+    stackedBarTooltip: 'Stacked',
+    normalizedBarTooltip: '100% Stacked',
+    pieTooltip: 'Pie',
+    doughnutTooltip: 'Doughnut',
+    lineTooltip: 'Line',
+    groupedAreaTooltip: 'Area',
+    stackedAreaTooltip: 'Stacked',
+    normalizedAreaTooltip: '100% Stacked',
+    scatterTooltip: 'Scatter',
+    bubbleTooltip: 'Bubble',
+    histogramTooltip: 'Histogram',
+    noDataToChart: 'No data available to be charted.',
+    pivotChartRequiresPivotMode: 'Pivot Chart requires Pivot Mode enabled.'
+}
